f161e6becdbcc572cd158ed6e94f900864cf6158
Frédéric Le Coquil <flecoqui@microsoft.com>
2024-06-17 08:20:51 +0200

    Add html sample

---
+++ src/html/test2/index.js
@@ -0,0 +1,11 @@
+document.addEventListener("keypress", function(event) {
+    if (event.key === "M") {
+      alert('Hello Mad World!');
+    }
+    else if (event.key === "B") {
+      alert('Hello Beautiful World!');
+    }
+    else{
+        alert('Hello World!');
+    }
+  });
---
+++ src/html/test3/index.js
@@ -0,0 +1,14 @@
+button=document.getElementById("statusButton")
+
+button.addEventListener("click", function(event) {
+    status = document.getElementById("statusId").value;
+    if (status ) {
+      alert('Hello World!');
+    }
+    else {
+      alert('Hello  World!');
+    }
+  });
+object.addEventListener("click", myScript);
+
+
